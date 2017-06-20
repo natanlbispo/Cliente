a63f8c5d444172a4f607609ae7f0eaf28edeeba4
bisponatan <natanbispo@dcc.ufba.br>
2017-06-20 11:07:01 -0300

    addittion home.php

---
+++ source-code/header.php
@@ -0,0 +1,36 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SABolsas</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+  </head>
+  <header>
+    <nav class="navbar navbar-inverse navbar-fixed-top" style="background-color:#000066">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed">
+          </button>
+          <a class="navbar-brand" href="home.php" >SABolsas - PGCOMP</a>
+          <a class="navbar-brand" href="home.php" > <span class="label label-default">Gerenciar</span></a>
+          <a class="navbar-brand" href="home.php" > <span class="label label-default">....</span></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <div id="navbar" class="navbar-collapse collapse">
+           <ul class="nav navbar-nav navbar-right">
+             <li><a href="sair.php">Sair</a></li>
+           </ul>
+         </div><!--/.nav-collapse -->
+       </div>
+        </div><!--/.navbar-collapse -->
+      </div>
+      <div class="navbar-collapse collapse">
+    </nav>
+</header>
